de17e6bc70779f4891c2c6484dc46ed97dd0bbd0
sanja-k-hristov <sanja.k.hristov@gmail.com>
2021-09-13 23:52:24 +0200

    homework1 added

---
+++ WebApplication1/WebApplication1/StaticDB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public static class StaticDB
+    {
+        public static List<string> UserNames = new List<string>
+        {
+            "Sanja",
+            "Jane",
+            "Oliver"
+        };
+    }
+}
